4e029d124a4c6fe8ecbcb9b934765f536a3947d6
Yaniv Savir <saviry@gmail.com>
2014-09-14 14:39:21 -0400

    fixed erroring when no params are passed

--- grips.js
+++ grips.js
@@ -2,7 +2,7 @@
 // needs tag attributes functionality
 Handlebars.registerHelper('linkTo', function(body, link, options){
   var params = {}
-  link.match(/\/:(\w+)/g).map(function(match){
+  if (link.match(/\/:(\w+)/g)) link.match(/\/:(\w+)/g).map(function(match){
     var param = match.slice(2, match.length)
     params[param] = options['hash']['params:' + param]
   })
