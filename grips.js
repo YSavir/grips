bb81c74811f320bc96d44a09fe0e6ea929f4b8b5
Yaniv Savir <saviry@gmail.com>
2014-09-14 13:10:10 -0400

    added linkTo

---
+++ grips.js
@@ -0,0 +1,14 @@
+// Adds an anchor tag to the DOM
+// needs tag attributes functionality
+Handlebars.registerHelper('linkTo', function(body, link, options){
+  var params = {}
+  link.match(/\/:[\w+]/g).map(function(match){
+    var param = match.slice(2, match.length)
+    params[param] = options['hash']['params' + param]
+  })
+  for (param in params){
+    link = link.replace(':' + param, params[param])
+  }
+  newAnchor = '<a href="' + link + '">' + body + '</a>'
+  return new Handlebars.SafeString(newAnchor)
+})
