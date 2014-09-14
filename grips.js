37dddcde514895bf3516ff44c360580814af6105
Yaniv Savir <saviry@gmail.com>
2014-09-14 13:57:29 -0400

    removed unnecessary variable usage

--- grips.js
+++ grips.js
@@ -4,11 +4,10 @@ Handlebars.registerHelper('linkTo', function(body, link, options){
   var params = {}
   link.match(/\/:(\w+)/g).map(function(match){
     var param = match.slice(2, match.length)
-    params[param] = options['hash']['params' + param]
+    params[param] = options['hash']['params:' + param]
   })
   for (param in params){
     link = link.replace(':' + param, params[param])
   }
-  newAnchor = '<a href="' + link + '">' + body + '</a>'
-  return new Handlebars.SafeString(newAnchor)
+  return new Handlebars.SafeString('<a href="' + link + '">' + body + '</a>')
 })
