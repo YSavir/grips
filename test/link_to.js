889ee769a45cdae88df700b48535d957f5fac7f0
Yaniv Savir <saviry@gmail.com>
2014-09-15 10:37:17 -0400

    added chai to tests

--- test/link_to.js
+++ test/link_to.js
@@ -1,9 +1,20 @@
-var assert = require('assert')
+var expect = chai.expect;
 
-describe('linkTo'), function(){
-  describe('base'), function(){
-    it('should return an anchor tag with a body and a href', function(){
-      assert.equal(Handlebars.templates['link_to/base']().string, '<a href="/">Home</a>')
-    })
-  }
-}
+describe('linkTo', function(){
+  describe('base', function(){
+    it('should return an anchor tag with body an an href', function(){
+      expect(Handlebars.templates['link_to/base']().string).to.equal('<a href="/">Home</a>');
+    });
+  });
+  describe('with  params', function(){
+    it('should return an anchor tag with the params interpolated into the path', function(){
+      expect(Handlebars.templates['link_to/params']({name: 'Yaniv', id: 1}).string).to.equal('<a href="/users/1">Yaniv</a>');
+    });
+  });
+  describe('with  attributes', function(){
+    it('should return an anchor tags with id and class attributes', function(){
+      expect(Handlebars.templates['link_to/attributes']().string).to.equal('<a class="test" id="linkToTest" href="/">Home</a>');
+    });
+  })
+});
+
