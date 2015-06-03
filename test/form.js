3228dc3845043a70f8f96797928bf4e5eb704737
Yaniv Savir <saviry@gmail.com>
2015-06-03 12:14:33 -0400

    moves templates into tests

--- test/form.js
+++ test/form.js
@@ -1,16 +1,31 @@
 var expect = chai.expect;
 
 describe('formTag', function(){
+
+  it('should raise an error if no HTTP method is given', function(){
+    var templateText = "{{#formTag this}}\n{{/formTag}}";
+    var template = Handlebars.compile(templateText);
+   
+    expect(template).to.throw('form must be given an HTTP method'); 
+  })
+
+  it('should raise an error if no action is given', function(){
+    var templateText = "{{#formTag this}}\n{{/formTag}}";
+    var template = Handlebars.compile(templateText);
+    var testFunction = function(){
+      return template({method: 'get'});
+    };
+    
+    expect(testFunction).to.throw('form must be given an action')
+  });
+
   describe('with only required arguments', function(){
-    var testTemplate = function(){return Handlebars.templates['form_tag/base']({model: {}, method: 'get', action: '/'})}
-    it('should raise an error if no method or action is given', function(){
-      var testTemplateForMethod = function(){Handlebars.templates['form_tag/base']()}
-      var testTemplateForAction = function(){Handlebars.templates['form_tag/base']({method: 'get'})}
-      expect(testTemplateForMethod).to.throw('form must be given an HTTP method'); 
-      expect(testTemplateForAction).to.throw('form must be given an action')
-    })
     it('should return a form field with an action and method', function(){
-      expect(testTemplate().string).to.equal('<form action="/" method="get"></form>')
+      var templateText = "{{#formTag this}}\n{{/formTag}}";
+      var template = Handlebars.compile(templateText);
+      var resultText = template({method: 'get', action: '/'}).string;
+
+      expect(resultText).to.equal('<form action="/" method="get"></form>')
     })
   })
 })
--- test/link_tag.js
+++ test/link_tag.js
@@ -3,17 +3,31 @@ var expect = chai.expect;
 describe('linkTag', function(){
   describe('base', function(){
     it('should return an anchor tag with body an an href', function(){
-      expect(Handlebars.templates['link_tag/base']().string).to.equal('<a href="/">Home</a>');
+      var templateText = "{{{linkTag 'Home' '/'}}}";
+      var template = Handlebars.compile(templateText);
+      var resultText = template().string;
+
+      expect(resultText).to.equal('<a href="/">Home</a>');
     });
   });
-  describe('with  params', function(){
+
+  describe('with params', function(){
     it('should return an anchor tag with the params interpolated into the path', function(){
-      expect(Handlebars.templates['link_tag/params']({name: 'Yaniv', id: 1}).string).to.equal('<a href="/users/1">Yaniv</a>');
+      var templateText = "{{{linkTag this.name '/users/:user_id' param:user_id=1}}}";
+      var template = Handlebars.compile(templateText);
+      var resultText = template({name: 'Yaniv', id: 1}).string;
+
+      expect(resultText).to.equal('<a href="/users/1">Yaniv</a>');
     });
   });
+
   describe('with  attributes', function(){
     it('should return an anchor tags with id and class attributes', function(){
-      expect(Handlebars.templates['link_tag/attributes']().string).to.equal('<a id="linkToTest" class="test" href="/">Home</a>');
+      var templateText = "{{{linkTag 'Home' '/' attr:class='test' attr:id='linkToTest' }}}";
+      var template = Handlebars.compile(templateText);
+      var resultText = template().string;
+
+      expect(resultText).to.equal('<a id="linkToTest" class="test" href="/">Home</a>');
     });
   })
 });
