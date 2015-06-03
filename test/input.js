e05060a7b7060ba0ae6c7cbd6311f3d5bb2ca66b
Yaniv Savir <saviry@gmail.com>
2015-06-03 12:14:33 -0400

    moves templates into tests

--- test/input.js
+++ test/input.js
@@ -3,12 +3,21 @@ var expect = chai.expect;
 describe('inputTag', function(){
   describe('with only required arguments', function(){
     it('should create an input with a name', function(){
-      expect(Handlebars.templates['input_tag/basic']().string).to.equal('<input name="name">')
+      var templateText = "{{{inputTag 'name'}}}";
+      var template = Handlebars.compile(templateText);
+      var resultText = template().string;
+
+      expect(resultText).to.equal('<input name="name">')
     })
   })
+
   describe('with optional attributes', function(){
     it('should create an input with attributes when given attributes', function(){
-      expect(Handlebars.templates['input_tag/attributes']().string).to.equal('<input id="input-test" class="test" name="name">')
+      var templateText = "{{{inputTag 'name' attr:class='test' attr:id='input-test'}}}";
+      var template = Handlebars.compile(templateText);
+      var resultText = template().string;
+
+      expect(resultText).to.equal('<input id="input-test" class="test" name="name">')
     })
   })
 })
