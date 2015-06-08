42c274c0ec0683c184b33dea1b8001cbb4c6a674
Yaniv Savir <saviry@gmail.com>
2015-06-08 12:49:27 -0400

    improves files structure

---
+++ test/form-elements/form.js
@@ -0,0 +1,31 @@
+var expect = chai.expect;
+
+describe('formTag', function(){
+
+//  it('should raise an error if no HTTP method is given', function(){
+//    var templateText = "{{#formTag this}}\n{{/formTag}}";
+//    var template = Handlebars.compile(templateText);
+//   
+//    expect(template).to.throw('form must be given an HTTP method'); 
+//  })
+//
+//  it('should raise an error if no action is given', function(){
+//    var templateText = "{{#formTag this}}\n{{/formTag}}";
+//    var template = Handlebars.compile(templateText);
+//    var testFunction = function(){
+//      return template({method: 'get'});
+//    };
+//    
+//    expect(testFunction).to.throw('form must be given an action')
+//  });
+//
+//  describe('with all required arguments', function(){
+//    it('should return a form field with an action and method', function(){
+//      var templateText = "{{#formTag this}}\n{{/formTag}}";
+//      var template = Handlebars.compile(templateText);
+//      var resultText = template({method: 'get', action: '/'}).string;
+//
+//      expect(resultText).to.equal('<form action="/" method="get"></form>')
+//    })
+//  })
+})
